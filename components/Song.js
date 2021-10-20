e3a9137e99e0f8407f84caa082c056ec757594d6
Brandon <brandonbland90@gmail.com>
2021-10-19 23:37:10 -0400

    Added popover menu for each song, and green styles for completed songs.

--- components/Song.js
+++ components/Song.js
@@ -1,5 +1,18 @@
+import { useState } from 'react'
 import { useDispatch } from 'react-redux'
-import { Button, TableRow, TableCell, Popover } from '@mui/material'
+import {
+  Divider,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Popover,
+  TableCell,
+  TableRow,
+} from '@mui/material'
+import CheckIcon from '@mui/icons-material/Check'
+import DeleteIcon from '@mui/icons-material/Delete'
 import MoreVertIcon from '@mui/icons-material/MoreVert'
 import moment from 'moment'
 
@@ -7,9 +20,11 @@ import styles from '../styles/Home.module.scss'
 import { deleteSong, updateSong } from '../actions/songActions'
 
 const Song = ({ songName, dueDate, id, isComplete }) => {
+  const [anchorEl, setAnchorEl] = useState(null)
   const dispatch = useDispatch()
   const onDeleteSong = () => {
     dispatch(deleteSong(id))
+    setAnchorEl(null)
   }
   const onToggleComplete = () => {
     const completedSong = {
@@ -19,27 +34,85 @@ const Song = ({ songName, dueDate, id, isComplete }) => {
       isComplete: !isComplete,
     }
     dispatch(updateSong(id, completedSong))
+    setAnchorEl(null)
   }
+
+  // Popover related functions
+  const handleClick = (e) => {
+    setAnchorEl(e.currentTarget)
+  }
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+  const open = Boolean(anchorEl)
+  const popoverId = open ? 'simple-popover' : undefined
+  // End popover functions
+
   return (
     <>
       <TableRow>
         <TableCell>
           <div className={styles.song}>
-            <span className={styles.songName}>{songName}</span>
+            <span className={isComplete && `${styles.songCompleted}`}>
+              {songName}
+            </span>
           </div>
         </TableCell>
-        <TableCell align="right">{moment(dueDate).format('l')}</TableCell>
+        <TableCell
+          align="right"
+          className={isComplete && `${styles.songCompleted}`}
+        >
+          {moment(dueDate).format('l')}
+        </TableCell>
         <TableCell align="right">
-          <Button
-            variant="contained"
-            color="success"
-            onClick={onToggleComplete}
+          <Popover
+            id={popoverId}
+            open={open}
+            anchorEl={anchorEl}
+            onClose={handleClose}
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'right',
+            }}
+            transformOrigin={{
+              vertical: 'center',
+              horizontal: 'right',
+            }}
           >
-            !
-          </Button>
-          <Button variant="contained" color="error" onClick={onDeleteSong}>
-            X
-          </Button>
+            <List disablePadding={true}>
+              <ListItem>
+                <ListItemText>
+                  <ListItemText>{songName}</ListItemText>
+                </ListItemText>
+              </ListItem>
+
+              <Divider />
+
+              <ListItem>
+                <ListItemButton onClick={onToggleComplete}>
+                  <ListItemIcon>
+                    <CheckIcon />
+                  </ListItemIcon>
+                  <ListItemText>
+                    Mark {isComplete ? 'Incomplete' : 'Complete'}
+                  </ListItemText>
+                </ListItemButton>
+              </ListItem>
+              <ListItem>
+                <ListItemButton onClick={onDeleteSong}>
+                  <ListItemIcon>
+                    <DeleteIcon />
+                  </ListItemIcon>
+                  <ListItemText>Delete</ListItemText>
+                </ListItemButton>
+              </ListItem>
+            </List>
+          </Popover>
+          <MoreVertIcon
+            aria-describedby={popoverId}
+            onClick={handleClick}
+            className={styles.menuToggle}
+          />
         </TableCell>
       </TableRow>
     </>
