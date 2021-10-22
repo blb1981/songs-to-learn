c1a2ffa0427000096fd1e16c6836063b41c47628
Brandon <brandonbland90@gmail.com>
2021-10-22 12:40:39 -0400

    Added formik to song form, has bugs

--- components/SongForm.js
+++ components/SongForm.js
@@ -4,68 +4,89 @@ import moment from 'moment'
 import { Button, TextField } from '@mui/material'
 import { DatePicker } from '@mui/lab'
 import { v4 as uuidv4 } from 'uuid'
+import { useFormik } from 'formik'
+import * as yup from 'yup'
 
 import styles from '../styles/Home.module.scss'
 import { addSong } from '../actions/songActions'
 
+const validationSchema = yup.object({
+  songName: yup.string().required('Required'),
+  dueDate: yup.date().required('Required'),
+})
+
 const SongForm = () => {
-  // Calculate one month from current date for default display in datepicker
   const monthFromNow = moment().add(1, 'M')
 
   const dispatch = useDispatch()
 
-  // Form data kept in local state, using React hooks
   const [songName, setSongName] = useState('')
   const [dueDate, setDueDate] = useState(monthFromNow)
 
-  // Handler for form submission to add song to list
-  const onAddSong = (e) => {
+  const handleAddSong = (e) => {
     e.preventDefault()
 
-    // Generate unique ID
     const id = uuidv4()
 
-    // Send data to redux store
     dispatch(addSong(songName, dueDate.format(), id))
 
-    // Reset form state after submission
     setSongName('')
     setDueDate(monthFromNow)
   }
+
+  const formik = useFormik({
+    initialValues: { songName, dueDate },
+    validationSchema,
+    onSubmit: (values) => {
+      console.log(values)
+    },
+  })
   return (
-    <form onSubmit={onAddSong} className={styles.form}>
-      <div className={styles.formControl}>
-        <TextField
-          label="Song name"
-          value={songName}
-          onChange={(e) => setSongName(e.target.value)}
-          className={styles.input}
-        />
-      </div>
-      <div className={styles.formControl}>
-        <DatePicker
-          label="Due date"
-          onChange={(dueDate) => {
-            setDueDate(dueDate)
-          }}
-          value={dueDate}
-          renderInput={(params) => (
-            <TextField className={styles.input} {...params} />
-          )}
-          showTodayButton
-        />
-      </div>
-      <div className={styles.formControl}>
-        <Button
-          type="submit"
-          variant="contained"
-          className={styles.input}
-          size="large"
-        >
-          Add Song
-        </Button>
-      </div>
-    </form>
+    <div
+    // initialValues={{ songName: '', dueDate }}
+    // validationSchema={Yup.object({
+    //   songName: Yup.string().required('Required'),
+    //   dueDate: Yup.date().required('Required'),
+    // })}
+    >
+      <form onSubmit={formik.handleSubmit} className={styles.form}>
+        <div className={styles.formControl}>
+          <TextField
+            label="Song name"
+            name="songName"
+            value={formik.values.songName}
+            onChange={formik.handleChange}
+            error={formik.touched.songName && Boolean(formik.errors.songName)}
+            helperText={formik.touched.songName && formik.errors.songName}
+            className={styles.input}
+          />
+        </div>
+        <div className={styles.formControl}>
+          <DatePicker
+            label="Due date"
+            name="dueDate"
+            value={formik.values.dueDate}
+            onChange={formik.handleChange}
+            error={formik.touched.dueDate && Boolean(formik.errors.dueDate)}
+            helperText={formik.touched.dueDate && formik.errors.dueDate}
+            renderInput={(params) => (
+              <TextField className={styles.input} {...params} />
+            )}
+            showTodayButton
+          />
+        </div>
+        <div className={styles.formControl}>
+          <Button
+            type="submit"
+            variant="contained"
+            className={styles.input}
+            size="large"
+          >
+            Add Song
+          </Button>
+        </div>
+      </form>
+    </div>
   )
 }
 
